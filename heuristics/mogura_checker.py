7aef2d06478ee47826b0630e63c94533daff69f5
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2020-03-13 00:09:21 +0900

    add history, and rate of out moles

--- heuristics/mogura_checker.py
+++ heuristics/mogura_checker.py
@@ -4,26 +4,44 @@ out_thre = 2.0
 
 
 class Mog_check:
-    def __init__(self, data):
-        self.window = data
+    def __init__(self, data, window_size):
+        self.data = data
+        #self.window =
+        self.win_size = window_size
+        self.state = np.zeros((1,7))
+        self.history = np.empty((1,7))
+        self.i = 0
+
+    def get_data(self):
+        self.window = data[self.i:self.i+self.win_size]
+        self.i = self.i + 1
 
     def out_height(self):
+        self.get_data()
         return np.mean(self.window,axis=0)
+
+    def push_history(self):
+        self.history = np.append(self.history,self.state,axis=0)
+        print(self.history,self.state)
 
     def out_state(self):
         print("True: height > threshold")
         h_data = self.out_height()
-        state = h_data>out_thre
-        return state
-        
+        self.state[0,:] = (h_data>out_thre).astype(np.int)
+        return self.state
+
+    def out_rate(self):
+        rate = float(np.sum(self.state))/7.0
+        return rate
 
 if __name__ == '__main__':
     data = np.loadtxt('test2.csv',delimiter=",")
     winsize = 4
     i=0
+    mogura = Mog_check(data,winsize)
     while(i<data.shape[0]):
-        mogura = Mog_check(data[i:i+winsize])
-        print(mogura.out_state())
-        print(i)
+        print(i,mogura.out_state())
+        print(mogura.push_history())
+        raw_input()
 
         i=i+1
