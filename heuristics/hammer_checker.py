87f24e0316b05fdf87f7714cc38bd6022a044a88
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2020-04-14 20:02:06 +0900

    push history of hit

--- heuristics/hammer_checker.py
+++ heuristics/hammer_checker.py
@@ -5,23 +5,23 @@ import numpy as np
 import datetime
 import matplotlib.pyplot as plt
 
-TIME_COL=6
+TIME_COL=6 #number of skipped cols
+TIME_COL_LEN = 3 #number of imported cols
 
 GRAPH_RANGE=150
 SKIP_RANGE=10
 
 CHECK_COL=1
 
-label = ['a']*GRAPH_RANGE
-
 class Hammer_check:
   def __init__(self, data,num_lines):
     self.data=data
     self.i = 0
+    self.hit_t = 0
     self.num_lines = num_lines
     self.plt_array=np.zeros((3,GRAPH_RANGE))
     self.plt_array_diff=np.zeros((3,GRAPH_RANGE))
-    self.hit_t = 0
+    self.hit_history = np.empty((1,TIME_COL_LEN))
 
     self.fig = plt.figure(figsize=(10, 4))
     self.ax = self.fig.add_subplot(1,1,1)
@@ -47,16 +47,21 @@ class Hammer_check:
   def hit_checker(self):
     if abs(self.plt_array_diff[1,-1]) > 1.5:
       if abs(self.i-self.hit_t)>300:
+        hit_time=np.zeros((1,TIME_COL_LEN))
+        hit_time[:,0]=self.data[self.i,TIME_COL]
+        hit_time[:,1]=self.data[self.i,TIME_COL+1]
+        hit_time[:,2]=self.data[self.i,TIME_COL+2]
+        if(self.hit_t==0):
+          self.hit_history = hit_time
+        else:
+          self.hit_history = np.append(self.hit_history,hit_time,axis=0)
         self.hit_t = self.i #hit_t; last hit time
-
         hit_time = datetime.datetime(year=2019,month=11,day=1,minute=int(self.data[self.i,TIME_COL]),second=int(self.data[self.i,TIME_COL+1]),microsecond=int(self.data[self.i,TIME_COL+2]))
-        print("__________________________________hit: ",hit_time)
       else:
         hit_time = None
     else:
       hit_time = None
     return hit_time
-
 
   def main(self):
 
@@ -73,8 +78,3 @@ if __name__ == '__main__':
   num_lines = sum(1 for line in open("./imu_test.csv"))
   hmr = Hammer_check(data,num_lines)
   hmr.main()
-
-#removed_split_time_container = np.zeros((3,split_t_count+1))
-#removed_split_time_container = split_time_container[:,:split_t_count]
-#np.savetxt(dir_path+str(name)+"/split_time_test2.csv",X=removed_split_time_container.T,delimiter=",",fmt="%d")
-#np.savetxt(dir_path+str(name)+"/split_time_test2.csv",X=split_time_container.T,delimiter=",",fmt="%d")
