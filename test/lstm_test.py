b76ce5b827019b98460c9639ad264e429509464a
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2020-10-08 22:56:26 +0900

    tested gauss function

--- test/lstm_test.py
+++ test/lstm_test.py
@@ -7,7 +7,6 @@ from sklearn.datasets import load_iris
 timesteps = 100
  
 def lstm_mkdat(username,number,lstm_data,lstm_data_y):
-
   data_x0 = np.loadtxt('./output/'+username+'_face_test_class_'+str(number)+'.csv',delimiter=",")
   data_z0 = np.zeros((data_x0.shape[0],1,1))
   data_z0[-1,:,:] = 1#emo_data[0,1]
@@ -19,11 +18,6 @@ def lstm_mkdat(username,number,lstm_data,lstm_data_y):
   data_dim = data_x.shape[1]
   print("data_dim",data_dim)
 
-  #lstm_data = []
-  #lstm_data_y = []
-
-#for i in range(100): #for repeat test
-  #for t in range(5):
   for i in range(length-2*timesteps,length-timesteps):
     lstm_data.append(data_x[i:i+timesteps])
     lstm_data_y.append(data_z[i+timesteps])
@@ -51,14 +45,14 @@ def lstm_learn(lstm_data_x,lstm_data_y,data_name):
   from keras.utils import np_utils
   from keras.optimizers import Adam
 
-  #モデル定義
+  #define model
   hidden = 100
   model = Sequential()
   model.add(LSTM(hidden, input_shape=(timesteps,data_dim), stateful=False, return_sequences=False))
   model.add(Dense(lstm_data_y.shape[1]))
   model.compile(loss="mean_squared_error", optimizer='adam')
 
-  #学習
+  #learning
   model.fit(lstm_data_x, lstm_data_y,
            batch_size=32,
            epochs=1,
@@ -66,26 +60,27 @@ def lstm_learn(lstm_data_x,lstm_data_y,data_name):
            )
   print("ok")
 
-  #保存と読み込み
+  #save model
   model.save(data_name)
-  #load_model = load_model("./output/sincos_model.h5")
 
-  '''
-  #予測
-  lstm_data_y_predict = model.predict(lstm_data_x)
+def gauss(x,sigma):
+  mu = 0
+  return np.exp(-x**2/(2*sigma**2))
 
-  plt.figure()
-
-  plt.plot(lstm_data_y[-150:-50, 0], lw=2,label="data")
-  plt.plot(lstm_data_y_predict[-150:-50, 0], '--', lw=2,label="predict")
-  plt.legend()
-
-  plt.show()
-  '''
+def easy_mkdat(username,number):
+  data_x0 = np.loadtxt('./output/'+username+'_face_test_class_'+str(number)+'.csv',delimiter=",")
+  x = np.linspace(-data_x0.shape[0],0,data_x0.shape[0])
+  data_z0 = np.zeros((data_x0.shape[0],1,1))
+  data_z0[:,0,0] = gauss(x,data_x0.shape[0]/100)
+  return data_z0
 
 if __name__ == "__main__":
-
   filename = '1107-1'
+
+  x,yline =easy_mkdat(filename,0)
+  plt.plot(x,yline)
+  plt.show()
+
   
   for i in range(3):
     lstm_data,lstm_data_y=lstm_mkdat(filename,i)
