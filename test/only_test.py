db541d0a155f5b82a0b416138401f0d95359dccd
kaz-mintan <kumagai@mizuuchi.lab.tuat.ac.jp>
2022-01-21 21:15:57 +0900

    just removed commentouted part

--- test/only_test.py
+++ test/only_test.py
@@ -5,53 +5,6 @@ import matplotlib.pyplot as plt
  
 from lstm_test import *
  
-'''
-def sin(T=500):
-   x = np.arange(0, 2*T+1)
-   y = np.arange(0, 2*T+1)
-   sin = np.sin(2.0 * np.pi * x / T).reshape(-1, 1)+np.sin(2.0 * np.pi * y / T).reshape(-1, 1)
-
-   return x,y,sin
-
-def ret_sin(x):
-   T=500
-   sin = np.sin(2.0 * np.pi * x / T).reshape(-1, 1)
-   return sin
- 
-   data_x = np.loadtxt('./output/'+username+'_face_test_class_4.csv',delimiter=",")
-   #emo_data = np.loadtxt('../test_csv/emotion_test.csv',delimiter=",")
-   df = pd.read_csv('../emo_questionnaire/'+username+'.csv',header=None)#,delimiter=",",dtype="unicode")
-   emo_data = np.zeros((df.shape[0],df.shape[1]-1),dtype=np.int)
-   emo_data = df.values[:,:5]
-   print(data_x.shape[0])
-   data_z = np.zeros((data_x.shape[0],1,1))
-   print(emo_data[0,1])
-   data_z[-1,:,:] = 1#emo_data[0,1]
-
-   timesteps = 100
-   length=data_x.shape[0]
-   data_dim = data_x.shape[1]
-   print("data_dim",data_dim)
- 
-   lstm_data = []
-   lstm_data_y = []
-   index_data = []
-   index_data_y = []
-
-   #for i in range(100): #for repeat test
-   for t in range(10):
-     for i in range(length-2*timesteps,length-timesteps):
-         lstm_data.append(data_x[i:i+timesteps])
-         lstm_data_y.append(data_z[i+timesteps])
-     print("last i ",i)
-   #i = 84477
-
-   lstm_data_x = np.array(lstm_data)
-   lstm_data_y = np.array(lstm_data_y)
-   lstm_data_x = lstm_data_x.reshape(lstm_data_x.shape[0],timesteps,-1)
-   lstm_data_y = lstm_data_y.reshape(lstm_data_y.shape[0],-1)
-'''
-
 def lstm_predict(model_path,lstm_data_x,lstm_data_y):
    from keras.models import Sequential, load_model
    from keras.layers.core import Dense, Activation
